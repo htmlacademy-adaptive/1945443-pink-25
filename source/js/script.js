6644c4d079fe069a5a1d7f06891ab7d94975f32d
valery makarov <valerythf@outlook.com>
2022-05-18 18:14:03 +0300

    Исправляет ошибки

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,9 @@
 const header = document.querySelector(".header");
 const menuBurger = document.querySelector(".header__burger");
-const menu = document.querySelector(".navigation");
+const menu = document.querySelector(".header__navigation");
 
 menuBurger.onclick = function() {
-  menu.classList.toggle("navigation--active");
+  menu.classList.toggle("header__navigation--active");
   header.classList.toggle("header--active");
+  menuBurger.classList.toggle("header__burger--active")
 }
